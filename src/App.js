d9df4138820c1771eb8d4f68993b8d97c44f1e38
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-10-03 13:51:45 +0100

    Created basic navigation bar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Experience from './pages/Experience'
+import Navbar from './components/Navbar'
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           {/*Route path ~ url element ~ page path should render */}
           <Route path="/" element={<Home />}></Route>
