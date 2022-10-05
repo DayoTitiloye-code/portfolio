2d038f7fd040144b030398a62dbf1f3540801551
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-10-05 13:03:19 +0100

    made about, SM links, CSS, skills section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,41 @@
 import React from 'react'
-
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import EmailIcon from '@mui/icons-material/Email'
+import '../styles/Home.css'
 function Home() {
-  return <div>Home</div>
+  return (
+    <div className="home">
+      <div className="about">
+        <h2>Hi, My Name is Dayo!</h2>
+        <div className="prompt">
+          <p>
+            A computer science and cyber security graduate seeking to launch a
+            career as a tech developer.
+          </p>
+          <GitHubIcon />
+          <LinkedInIcon />
+          <EmailIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1> Skills </h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Front-End </h2>
+            <span>
+              React, HTML, CSS, React Native, NPM, Bootstrap, MaterialUI,
+              JavaScript
+            </span>
+          </li>
+          <li className="item">
+            <h2> Back-End </h2>
+            <span>NodeJS, MySQL, ExpressJS</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  )
 }
 
 export default Home
